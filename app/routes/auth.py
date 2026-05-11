46f4b742a8f9964402fd7bfef4fa5b7909452c55
panchangam supriya <panchangamssupriya4@gmail.com>
2026-05-11 17:51:50 +0530

    fixed login authentication

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -7,7 +7,6 @@ from app.models.user import User
 from app.schemas.user import UserCreate, UserLogin
 from app.utils.jwt_handler import create_access_token
 
-
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
@@ -18,20 +17,15 @@ pwd_context = CryptContext(
     deprecated="auto"
 )
 
-
 # -------------------------------
 # REGISTER
 # -------------------------------
 
 @router.post("/register")
 def register_user(
-
     user: UserCreate,
-
     role: str = "user",
-
     db: Session = Depends(get_db)
-
 ):
 
     existing_user = db.query(User).filter(
@@ -39,39 +33,27 @@ def register_user(
     ).first()
 
     if existing_user:
-
         raise HTTPException(
             status_code=400,
             detail="Email already registered"
         )
 
-    hashed_password = pwd_context.hash(
-        user.password
-    )
+    hashed_password = pwd_context.hash(user.password)
 
     new_user = User(
-
         username=user.username,
-
         email=user.email,
-
         hashed_password=hashed_password,
-
         role=role
     )
 
     db.add(new_user)
-
     db.commit()
-
     db.refresh(new_user)
 
     return {
-
         "message": "User registered successfully",
-
         "data": {
-
             "id": new_user.id,
             "username": new_user.username,
             "email": new_user.email,
@@ -79,18 +61,14 @@ def register_user(
         }
     }
 
-
 # -------------------------------
 # LOGIN
 # -------------------------------
 
 @router.post("/login")
 def login_user(
-
     user: UserLogin,
-
     db: Session = Depends(get_db)
-
 ):
 
     existing_user = db.query(User).filter(
@@ -98,7 +76,6 @@ def login_user(
     ).first()
 
     if not existing_user:
-
         raise HTTPException(
             status_code=400,
             detail="Invalid email or password"
@@ -110,31 +87,24 @@ def login_user(
     )
 
     if not valid_password:
-
         raise HTTPException(
             status_code=400,
             detail="Invalid email or password"
         )
 
     token = create_access_token({
-
-    "username": user.username,
-    "role": user.role
-
-})
+        "username": existing_user.username,
+        "role": existing_user.role
+    })
 
     return {
-
-    "message": "Login successful",
-
-    "access_token": token,
-
-    "token_type": "bearer",
-
-    "user": {
-
-        "id": user.id,
-        "username": user.username,
-        "role": user.role
+        "message": "Login successful",
+        "access_token": token,
+        "token_type": "bearer",
+        "user": {
+            "id": existing_user.id,
+            "username": existing_user.username,
+            "email": existing_user.email,
+            "role": existing_user.role
+        }
     }
-}
